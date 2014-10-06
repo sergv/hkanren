75cb95d0a97187793ca406abc72dac244057566b
Danny Gratzer <jozefg@cmu.edu>
2014-10-06 12:50:06 -0400

    Added top level module

---
+++ src/Language/DSKanren.hs
@@ -0,0 +1,4 @@
+module Language.DSKanren ( module Language.DSKanren.Core
+                         , module Language.DSKanren.Sugar ) where
+import Language.DSKanren.Core
+import Language.DSKanren.Sugar
