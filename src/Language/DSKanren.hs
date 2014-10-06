c03719f1585164f41275e030d71b44bf01f43c26
Danny Gratzer <jozefg@cmu.edu>
2014-10-06 12:51:44 -0400

    Added a simple module header

--- src/Language/DSKanren.hs
+++ src/Language/DSKanren.hs
@@ -1,3 +1,7 @@
+-- | DSKanren is a small implementation of the miniKanren
+-- langauge. The toplevel module exports the core language, in
+-- 'Language.DSKanren.Core' and some simple combinators from
+-- 'Language.DSKanren.Sugar'.
 module Language.DSKanren ( module Language.DSKanren.Core
                          , module Language.DSKanren.Sugar ) where
 import Language.DSKanren.Core
