7a5c41618ec6deb6d6185cab227c861c103cee76
Danny Gratzer <jozefg@cmu.edu>
2014-10-06 23:24:01 -0400

    Added our old header back into DSKanren.hs

--- src/Language/DSKanren.hs
+++ src/Language/DSKanren.hs
@@ -1,12 +1,7 @@
+-- | The toplevel module exports the core language, in
+-- 'Language.DSKanren.Core' and some simple combinators from
+-- 'Language.DSKanren.Sugar'.
 module Language.DSKanren ( module Language.DSKanren.Core
                          , module Language.DSKanren.Sugar ) where
 import Language.DSKanren.Core
 import Language.DSKanren.Sugar
-
-appendo :: Term -> Term -> Term -> Predicate
-appendo l r o =
-  conde [ program [l === Integer 0,  o === r]
-        , manyFresh $ \h t o' ->
-            program [ Pair h t === l
-                    , appendo t r o'
-                    , Pair h o' === o ]]
