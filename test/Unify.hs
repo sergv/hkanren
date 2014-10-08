ac1fa9d9b53d3f3e56e8e72ae9fdd9f4737f7deb
Danny Gratzer <jozefg@cmu.edu>
2014-10-08 16:21:05 -0400

    Added tests for conj, somehow it's not commutative!

--- test/Unify.hs
+++ test/Unify.hs
@@ -6,34 +6,49 @@ import Test.Tasty
 import Test.Tasty.QuickCheck hiding ((===))
 import QuickCheckHelper
 
-reflexive :: TestTree
-reflexive = testProperty "Reflexivity"
+eqrefl :: TestTree
+eqrefl = testProperty "Reflexivity"
             . forAll (mkTerm [currentGoal])
             $ \t -> hasSolution (t === t)
 
-commutative :: TestTree
-commutative = testProperty "Commutative"
-              . forAll (two $ mkTerm [currentGoal])
-              $ \ (l, r) ->
-                 hasSolution (l === r)
-                 ==> hasSolution (r === l)
+eqcomm :: TestTree
+eqcomm = testProperty "Commutative"
+         . forAll (two $ mkTerm [currentGoal])
+         $ \ (l, r) ->
+            hasSolution (l === r)
+            ==> hasSolution (r === l)
 
-trans :: TestTree
-trans = testProperty "Transitive"
-        . forAll (three $ mkTerm [currentGoal])
-        $ \(l, m, r) ->
-           hasSolution (conj (l === m) (m === r))
-           ==> hasSolution (r === l)
+eqtrans :: TestTree
+eqtrans = testProperty "Transitive"
+          . forAll (three $ mkTerm [currentGoal])
+          $ \(l, m, r) ->
+             hasSolution (conj (l === m) (m === r))
+             ==> hasSolution (r === l)
 
-closedFresh :: TestTree
-closedFresh = testProperty "Closed Under Fresh"
+eqTests :: TestTree
+eqTests = testGroup "Equality" [eqrefl, eqcomm, eqtrans]
+
+freshClosed :: TestTree
+freshClosed = testProperty "Closed Under Fresh"
               . forAll (Blind <$> mkPred [currentGoal])
               $ \(Blind p) ->
                  hasSolution p ==> hasSolution (fresh $ const p)
 
+freshTests :: TestTree
+freshTests = testGroup "Fresh" [freshClosed]
+
+conjcomm :: TestTree
+conjcomm = testProperty "Commutative"
+              . forAll (two $ Blind <$> mkPred [currentGoal])
+              $ \(Blind p, Blind o) ->
+                 hasSolution (conj p o)
+                 ==> hasSolution (conj p o)
+
+conjTests :: TestTree
+conjTests = testGroup "Conj" [conjcomm]
+
 main :: IO ()
 main = defaultMain . testGroup "List Tests" $
-       [ reflexive
-       , commutative
-       , trans
-       , closedFresh]
+       [ eqTests
+       , freshTests
+       , conjTests]
